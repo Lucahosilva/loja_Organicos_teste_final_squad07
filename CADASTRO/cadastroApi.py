1b33c3f54eba625f7b0dba05fc70faae9ffde3a8
divino <divino.junior@luizalabs.com>
2022-11-29 17:28:58 -0500

    Fizemos a conexão com mongo.

--- CADASTRO/cadastroApi.py
+++ CADASTRO/cadastroApi.py
@@ -4,10 +4,8 @@ import configuração
 conexao = MongoClient(configuração.conexaoMongo)
 db = conexao['Catalogo']
 
-teste = {"teste": "testando"}
-db['Produtos'].insert_one(teste)
-
-cursor = db['Produtos'].find()
-resultado_db = list(cursor)
-print(resultado_db)
+def consulta_catalogo():
+    cursor = db['Produtos'].find()
+    resultado_db = list(cursor)
+    return resultado_db
 
