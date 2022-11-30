f7328b6a2fac85f7e2d10c47daca0ce68a926a64
Bbruno Ccoelho <brunoc510@hotmail.com>
2022-11-30 13:05:39 -0300

    up

--- Carrinho/carrinho.py
+++ Carrinho/carrinho.py
@@ -20,17 +20,29 @@ conexao.rollback()
 
 '''
 import sqlite3 as sql
+
 conexao = sql.connect('cart.db')
 cursor = conexao.cursor()
 
 ## criar taela 
 def cria_table():
-    cursor.execute('CREATE TABLE IF NOT EXISTS carrinho (id_prod INTEGER PRIMARY KEY, Nome TEXT NOT NULL, Preço REAL NOT NULL, Quantidade INTEGER NOT NULL, Descrição TEXT NOT NULL)') 
+    try:
+        cursor.execute('CREATE TABLE IF NOT EXISTS carrinho (id_prod INTEGER PRIMARY KEY, Nome TEXT NOT NULL, Preço REAL NOT NULL, Quantidade INTEGER NOT NULL, Descrição TEXT NOT NULL)') 
+        print("Tabela criada com sucesso!")
+    except sql.Error as erro:
+        print("Tabela ja cadastrada:", erro) 
+        conexao.close()
+#cria_table()
 
 ## insere produto 
 def inser_prod():
-    cursor.execute("INSERT INTO carrinho VALUES(5,'cacau',12,5,'Do Brasil')")
-    conexao.commit()
+    try:
+        cursor.execute("INSERT INTO carrinho VALUES(5,'cacau',12,5,'Do Brasil')")
+        conexao.commit()
+    except sql.Error as erro:
+        print("Erro ao adicionar produto, verificar parametros.", erro) 
+        conexao.close()
+#inser_prod()
 
 ## Adição de produtos
 def inser_vprod():
@@ -42,31 +54,47 @@ def inser_vprod():
        (5,'Café',8,5,'Do Brasil'),
        (6,'Vinho',15,2,'Da Argentina'),
     ]
-    cursor.executemany('INSERT INTO carrinho VALUES(?,?,?,?,?)', data)
-    conexao.commit()
-    resultado = cursor.execute("select * from carrinho")
-    for linha in resultado:
-        print(linha)
-
+    try:
+        cursor.executemany('INSERT INTO carrinho VALUES(?,?,?,?,?)', data)
+        conexao.commit()
+        resultado = cursor.execute("select * from carrinho")
+        for linha in resultado:
+            print(linha)
+    except sql.Error as erro:
+        print("Erro ao adicionar produto'(s)', verificar parametros.", erro) 
+        conexao.close()
+#inser_vprod()
 
 ## Remoção de produtos
 def remove_prod():
-    cursor.execute("")
+    try:
+        cursor.execute("DELETE from carrinho WHERE id_prod = '2' ")
+        conexao.commit()
+        resultado = cursor.execute("select * from carrinho")
+        for linha in resultado:
+            print(linha)
+            print("ITEM EXCLUIDO COM SUCESSO!")
+        conexao.close()
 
-
-
-    resultado = cursor.execute("select * from carrinho")
-    for linha in resultado:
-        print(linha)
+    except sql.Error as erro:
+        print("Erro ao excluir o produto:", erro) 
+        conexao.close()
+#remove_prod()
 
 ## Alteração de quantidade
 def mpreco():
     pass
+#mpreco()
 
 ## Consulta de itens - pelo ID
 def consulta():
-    resultado = cursor.execute("select * from carrinho where id_prod ='2' ")
-    for linha in resultado:
-        print()
-        print("Produto selecionado:")
-        print(linha)
+    try:
+        resultado = cursor.execute("select * from carrinho where id_prod ='2' ")
+        for linha in resultado:
+            print()
+            print("Produto selecionado:")
+            print(linha)
+    except sql.Error as erro:
+        print("Banco de dados vazio:", erro) 
+        conexao.close()
+#consulta()
