ab32c9dc65e73096a69fcfa9501bb583029f5943
Bbruno Ccoelho <brunoc510@hotmail.com>
2022-11-30 15:33:14 -0300

    rotas main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sqlite3 as sql
 import pandas as pd
 import datetime
+from flask import Flask, redirect, url_for, request, render_template
 
+app = Flask(__name__)
 
 #----- topico 3. carrinho 
 #connection to db
@@ -19,3 +21,39 @@ print(teste)
 
 #close connection 
 conn.close()
+
+# ROTAS
+
+@app.route('/')
+def method_name():
+
+    pass
+
+@app.route('/route_name')
+def method_name():
+    pass
+
+@app.route('/route_name')
+def method_name():
+    pass
+
+@app.route('/route_name')
+def method_name():
+    pass
+
+@app.route('/')
+def method_name():
+    try:
+        resultado = cursor.execute("select * from carrinho where id_prod ='2' ")
+        for linha in resultado:
+            print()
+            print("Produto selecionado:")
+            print(linha)
+    except sql.Error as erro:
+        print("Banco de dados vazio:", erro) 
+        conn.close()
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
