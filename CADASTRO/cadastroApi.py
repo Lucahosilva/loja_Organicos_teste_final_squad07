e71f704052dd9acfb62178a17d7ae9674dfa7b8b
divino <divino.junior@luizalabs.com>
2022-11-30 16:13:22 -0500

    Foram criado duas funções:  cadastro e alteração.

--- CADASTRO/cadastroApi.py
+++ CADASTRO/cadastroApi.py
@@ -1,11 +1,36 @@
 from pymongo import MongoClient
-import configuração
+import configuracao
 
-conexao = MongoClient(configuração.conexaoMongo)
+conexao = MongoClient(configuracao.conexaoMongo)
 db = conexao['Catalogo']
 
 def consulta_catalogo():
     cursor = db['Produtos'].find()
     resultado_db = list(cursor)
+    print(resultado_db)
     return resultado_db
 
+def cadastro(parametros):
+    padrao = ["id", "nome", "valor", "descricao"]
+    if set(padrao).issubset(set(parametros.keys())): 
+        db["Produtos"].insert_one(parametros)
+        print('Produto cadastrado com sucesso!')
+    else:
+        print("Parâmetros insuficientes, para cadastro os seguintes itens são obrigatórios:  {'id': x, 'nome': x, 'valor': x, 'descricao': x}")
+        
+
+def alterar_produto_pelo_nome(parametros):
+    if not parametros:
+        print("Parâmetros insuficientes, para cadastro os seguintes itens são obrigatórios:  {'nome': x, 'Campo alterado': 'Alteração'}")
+    elif "nome" not in parametros:
+        print("Parametros insuficientes, chave: 'nome' não encontrada!")
+    else:
+        for chave, valor in parametros.items():
+            db['Produtos'].update_one(
+                {'nome': parametros['nome']},
+                {'$set':
+                    {chave: valor}
+                }
+            )
+                
+consulta_catalogo()
