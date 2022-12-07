7435e9b61af49a176758eef6274b88b3ff1920b3
Lucas Silva <lucashosilva@gmail.com>
2022-12-07 18:13:58 -0300

    modificações na parte do sqlite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sqlite3 as sql
 from json import dumps
-from flask import Flask
+from flask import Flask, make_response
 
 app= Flask(__name__)
 banco = "cart.db"
@@ -10,10 +10,10 @@ banco = "cart.db"
 criar_table = "CREATE TABLE IF NOT EXISTS Carrinho (id_prod INTEGER PRIMARY KEY, Nome TEXT NOT NULL, Preço REAL NOT NULL, Quantidade INTEGER NOT NULL, Descrição TEXT NOT NULL);"
 select_todos = "SELECT * FROM Carrinho;"
 truncate = "DELETE FROM Carrinho;" #"TRUNCATE TABLE carrinho;"
-#select_id = "SELECT * FROM Carrinho WHERE id_prod like ?;"
-#delete_id = "DELETE FROM Carrinho WHERE id_prod = ?;"
+select_id = "SELECT * FROM Carrinho WHERE id_prod like ?;"
+delete_id = "DELETE FROM Carrinho WHERE id_prod = ?;"
 #inserir_prod = "INSERT INTO Carrinho VALUES (:id,:Nome,:Preco,:Quantidade,:Descrição,);"
-atualiza_prod = "UPDATE carrinho SET quantidade = [valor] WHERE id_prod = [id_produto];" 
+atualiza_prod = "UPDATE Carrinho SET Quantidade = ? WHERE id_prod like ?;" 
 
 
 def abrir_conexao(banco):
@@ -27,7 +27,7 @@ def fechar_conexao(conn):
 
 @app.get('/') # FUNCIONANDO
 def home(): 
-    return ("SEJA BEM VINDO(A) A API ORGANICOS "), 201
+    return ("SEJA BEM VINDO(A) A API ORGANICOS "), 200
 
 @app.post('/criar_tabela') # FUNCIONADO
 def criar_tabela(): 
@@ -41,17 +41,25 @@ def consulta_tabela():
     conn, cursor = abrir_conexao(banco) # abertura do banco
     resultado = cursor.execute(select_todos).fetchall() 
     fechar_conexao(conn)
-    return {'Produtos': f'{resultado}'}
+    return {'Produtos': f'{resultado}'}, 200
 
-@app.put('/update/{id_prod}/{Quantidade}') # testando - ERROR 404 - Validation Error
-def update_prod_id(id_prod,Quantidade):
+@app.get('/consulta/<id_prod>') #Funcionando 
+def consulta_id(id_prod):
+    conn, cursor = abrir_conexao(banco) # abertura do banco
+    resultado = cursor.execute(select_id, [id_prod]).fetchall() 
+    fechar_conexao(conn)
+    return {'Produtos': f'{resultado}'}, 200
+
+@app.put('/update/<id_prod>/<Quantidade>') # testando - Executa como se estivesse correto porem não altera o banco
+def update_set_quantidade(id_prod,Quantidade):
     try:
         conn, cursor = abrir_conexao(banco)
-        cursor.execute('UPDATE carrinho SET Quantidade = [Quantidade] WHERE id_prod = [id_prod]')
+        cursor.execute(atualiza_prod, [Quantidade, id_prod])
+        return (f'id {id_prod} - Quantidade alterada para {Quantidade} com sucesso'), 202
     except sql.Error as erro:
         resultado = erro
         fechar_conexao(conn)
-        return (f'Erro ao adicionar produto(s), verificar parametros {resultado}')
+        return (f'Erro ao adicionar produto(s), verificar parametros {resultado}'), 400
 
 @app.delete('/deleta') # FUNCIONANDO
 def deleta_tabela():
@@ -60,25 +68,26 @@ def deleta_tabela():
     fechar_conexao(conexao)
     return {'message': 'Carrinho apagado!'}, 204
 
-# EXTRA ADICONAR DADOS
+@app.delete('/deleta_id/<id_prod>') # FUNCIONANDO, MAS A MANSAGEM DE RETURN NÃO VOLTA
+def deleta_id(id_prod):
+    conexao, cursor = abrir_conexao(banco)
+    cursor.execute(delete_id, [id_prod])
+    fechar_conexao(conexao)
+    return {'mensagem': 'produto deletado com sucesso'}, 204
 
-@app.post('/alimentar') # FUNCIONANDO
-def alimentar_tabela():
+
+@app.post('/alimentar/<id_prod>/<name>/<value>/<quantity>/<desc>') # FUNCIONANDO
+def alimentar_tabela(id_prod, name, value, quantity, desc):
     try:
         conn, cursor = abrir_conexao(banco)
         data = [
-        (1,'Cacau',12.5,3,'Do Brasil'),
-        (2,'Arroz',10,2,'Do Brasil'),
-        (3,'Feijão',8,5,'Do Brasil'),
-        (4,'Batata',11,10,'Do Brasil'),
-        (5,'Café',8,5,'Do Brasil'),
-        (6,'Vinho',15,2,'Da Argentina'),
+        #(1,'Cacau',12.5,3,'Do Brasil'),
+        (id_prod,name,value,quantity,desc)
         ]
         cursor.executemany('INSERT INTO Carrinho VALUES(?,?,?,?,?)', data)
         resultado = cursor.execute(select_todos)
-        print(resultado)
         fechar_conexao(conn)
-        return("Cadastrado") , 202
+        return(f"{name} Cadastrado com sucesso") , 202
     except sql.Error as erro:
         resultado = erro
         fechar_conexao(conn)
