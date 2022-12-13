d0c2a32ee01e694bbeb214b943f7028cd2389785
William <willianch3@gmail.com>
2022-12-13 16:38:42 -0300

    commit testes

--- relatorio/test_relatorio.py
+++ relatorio/test_relatorio.py
@@ -1,18 +1,15 @@
-<<<<<<< HEAD
+
 from main import app
 import pytest
 
 
-=======
 from relatorio.main import app
 import pytest
 
->>>>>>> cd6f35baf28c03d75e64dcba7424f0f5245bab7e
 @pytest.fixture()
 def client():
     return app.test_client()
 
-<<<<<<< HEAD
 
 def test_sales(client):
     response = client.get("/sales")
@@ -47,8 +44,8 @@ def test_get_iten(client):
     response = client.get("/get_iten/tomate")
     assert response.status_code == 200 
     assert '{"Menssage": "iten not found"}' not in response.text
-=======
+
+
 def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
->>>>>>> cd6f35baf28c03d75e64dcba7424f0f5245bab7e
