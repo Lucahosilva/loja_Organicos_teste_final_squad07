2df7e22592e250c9d4cfd58c13882457f744ee39
Lucas Silva <lucashosilva@gmail.com>
2022-12-16 17:33:01 -0300

    Refactor and applied good praticies at carrinho folder

--- carrinho/main.py
+++ carrinho/main.py
@@ -1,18 +1,16 @@
-import sqlite3 as sql
-from json import dumps
 from flask import Flask
+from json import dumps
+import sqlite3 as sql
 
 app= Flask(__name__)
 banco = "cart.db"
 #
 # Comandos SQL
-# contagem = "SELECT COUNT(*) FROM Carrinho;"
 criar_table = "CREATE TABLE IF NOT EXISTS carrinho (id_prod INTEGER PRIMARY KEY, nome TEXT NOT NULL, preco REAL NOT NULL, quantidade INTEGER NOT NULL, desc TEXT NOT NULL);"
 select_todos = "SELECT * FROM carrinho;"
-truncate = "DELETE FROM carrinho;" #"TRUNCATE TABLE carrinho;"
+truncate = "DELETE FROM carrinho;" #"TRUNCATE 
 select_id = "SELECT * FROM carrinho WHERE id_prod like ?;"
 delete_id = "DELETE FROM carrinho WHERE id_prod = ?;"
-#inserir_prod = "INSERT INTO Carrinho VALUES (:id,:Nome,:Preco,:Quantidade,:Descrição,);"
 atualiza_prod = "UPDATE carrinho SET quantidade = (quantidade) WHERE id_prod = (id_prod);" 
 
 def abrir_conexao(banco):
@@ -52,19 +50,19 @@ def listar_produto(id_prod):
     except sql.Error as erro:
         resultado = erro
         fechar_conexao(conexao)
-        return (f'Erro ao consultar produto {id_prod}, verificar parametros {resultado}'), 400
+        return {f'Erro ao consultar produto {id_prod}, verificar parametros {resultado}'}, 400
 
 @app.put('/atualiza/<id_prod>/<quantidade>') #Funcionando
 def update_quanti(id_prod,quantidade): # /atualiza/2/20
-    # try:
+    try:
         conexao, cursor = abrir_conexao(banco)
         cursor.execute(f"UPDATE carrinho SET quantidade = {quantidade} WHERE id_prod = {id_prod}")   
         fechar_conexao(conexao)
-        return (f'id {id_prod} - Quantidade alterada para {quantidade} com sucesso'), 202
-    # except sql.Error as erro:
-    #     resultado = erro
-    #     fechar_conexao(conexao)
-    #     return (f'Erro ao atulizar produto {id_prod}, verificar parametros {resultado}')#, 400
+        return {f'id {id_prod} - Quantidade alterada para {quantidade} com sucesso'}, 202
+    except sql.Error as erro:
+        resultado = erro
+        fechar_conexao(conexao)
+        return {f'Erro ao atulizar produto {id_prod}, verificar parametros {resultado}'}, 400
 
 @app.delete('/deletar') # FUNCIONANDO 
 def deleta_tabela():
@@ -90,11 +88,11 @@ def alimentar_tabela(id_prod, name, value, quantity, desc): # /alimentar/1/Carro
         cursor.executemany('INSERT INTO carrinho VALUES(?,?,?,?,?)', data)
         resultado = cursor.execute(select_todos)
         fechar_conexao(conexao)
-        return(f"{name} Cadastrado com sucesso") , 202
+        return{f"{name} Cadastrado com sucesso"} , 202
     except sql.Error as erro:
         resultado = erro
         fechar_conexao(conexao)
-        return (f'Erro ao adicionar produto(s), verificar parametros {resultado}'), 400
+        return {f'Erro ao adicionar produto(s), verificar parametros {resultado}'}, 400
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
